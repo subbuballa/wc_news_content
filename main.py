69be5a37632fbb3b9da84d195fba7f6caa49d7f8
subbu <subbu.balla@gmail.com>
2019-07-02 07:40:21 -0500

    c

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
 import feedparser
+from mylocalpackage.repository import FeedRepository, DomainRepository
+import json
+import datetime
 
+
+def extract_info(parsed_feed):
+    extracted_data = []
+    for entry in parse_feeds['entries']:
+        extracted_data.append({
+            "url": entry['feedburner_origlink'],
+            "time": datetime.datetime.strptime(entry['published'])
+        })
+
+def parse_feeds():
+    repo = FeedRepository()
+    domain_repo = DomainRepository()
+    active_feeds = repo.get_active_feed_urls('time')
+    for feed_url in active_feeds:
+        if feed_url.get('etag', '') != '':
+            parsed_feed = feedparser.parse(feed_url['topic_url'])
+        else:
+            parsed_feed = feedparser.parse(feed_url['topic_url'], etag=feed_url['etag'])
+        # print(parsed_feed)
+        # f = open('output.json', 'a')
+        # f.write(json.dumps(parsed_feed))
+        # f.close()
+        if parsed_feed['status'] == 200:
+            data = extract_info(parsed_feed)
+            print(data)
+            # domain_repo.save_extracted_url_with_content('time', data)
+            feed_url['etag'] = parsed_feed['etag']
+    print(active_feeds)
+    # update etags to the urls
+    repo.save_feed_topics_by_domain('time', active_feeds)
+
+if __name__ == "__main__":
+    parse_feeds()
--- mylocalpackage/repository.py
+++ mylocalpackage/repository.py
@@ -5,10 +5,31 @@ class FeedRepository():
     def save_feed_topics_by_domain(self, domain_name, new_data):
         db = firestore.Client()
         topic_doc_ref = db.collection(u'feeds').document(domain_name)
-        topic_doc_ref.set(new_data, merge=True)
+        data = {
+            'name': domain_name,
+            'topics': new_data
+        }
+        topic_doc_ref.set(data, merge=True)
 
     def get_active_feed_urls(self, domain_name, is_active=False):
         db = firestore.Client()
         topic_doc_ref = db.collection(u'feeds').document(domain_name)
         topics = topic_doc_ref.get().to_dict()
         return [topic for topic in topics['topics'] if topic['is_active'] == is_active]
+
+
+class DomainRepository():
+    def get_new_urls(self, domain_name):
+        db = firestore.Client()
+        new_urls_doc = db.collection(u'domains').document(domain_name)
+        new_urls_doc_results = new_urls_doc.get()
+        return new_urls_doc_results.to_dict()
+
+    def save_extracted_url_with_content(self, domain_name, new_data):
+        db = firestore.Client()
+        new_urls_doc = db.collection(u'domains').document(domain_name)
+        data = {
+            'name': domain_name,
+            'new': new_data
+        }
+        new_urls_doc.set(data, merge=True)
